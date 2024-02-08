7d88bd363ea97e698d60f5473f3d25ef634725ef
Elias Hagos <eliasars@yahoo.com>
2024-02-08 22:19:44 +0200

    my commit

---
+++ index.js
@@ -0,0 +1,68 @@
+
+let temperature = 6;
+console.log(`The temperature is ${temperature}`);
+ 
+if (temperature < 46) {
+   console.log("It's warm outside");
+
+}
+
+//  Make the following code to give a 'true' boolean value by changing the code.
+/*let num = 0;
+if (1 > 2) {
+   num = 1;
+}
+console.log(num === 1);*/
+ 
+let num = 0;
+if (1 < 2) {
+   num = 1;
+}
+console.log(num === 1);
+//
+
+let text = 'hello';
+if (text === 'hello') {
+   text = text + ' world';
+}
+console.log(text === 'hello world');
+
+/* Concatenation*/
+
+var x = 'I like' + ' ' + 'cats.';
+console.log(x);
+/*Strings provide a lot of HELPER METHODS,
+--> commonly used helpers.
+
+toUpperCase and toLowerCase - change the case of all characters
+trim - remove whitespace at the beginning and end
+includes, startsWith and endsWith - determine whether another string is part of the given string
+slice - extract a section of the string*/
+
+/* LOOPS!
+While Loops
+With a while loop, you can execute code repeatably as long as a certain condition is fulfilled. It is written with the while keyword followed by a condition wrapped in round brackets and a code block that contains the body of the loop wrapped in curly brackets.
+
+while (condition) {
+  // code that is executed repeatedly as long as the condition is true
+}
+
+do {
+  // The code here will always be executed once and then
+  // repeatedly while the condition is true.
+} while (condition);
+
+switch (x) {
+  case option1:
+    // code that is executed when "x === option1" is true
+    break;
+  case option2:
+    // code that is executed when "x === option2" is true
+    break;
+  default:
+    // code that is executed when x does not equal any of the options
+}
+
+
+*/
+
